647af268884b65b92f4f8fca3ed7d5ec72f6d7a1
gujerbit <ghftm1124@gmail.com>
2021-09-28 13:53:13 +0900

    Spring boot & vue-cli 4 초기 설정 (2021-09-28)

---
+++ src/main/java/com/gujerbit/encrypt/EncryptTestApplication.java
@@ -0,0 +1,13 @@
+package com.gujerbit.encrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncryptTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gujerbit/encrypt/EncryptTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gujerbit.encrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
