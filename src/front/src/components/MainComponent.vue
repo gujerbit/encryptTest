fa535c68b2905f648e6d5c65c54bd97fae4102bd
gujerbit <ghftm1124@gmail.com>
2021-09-29 12:15:14 +0900

    AES-CBC&AES-GCM 변경: AES-CBC&AES-GCM의 encrypt result를 Base64 -> Hex로 변경하였습니다, 이제 iv값과 secret key값을 사용자에게 받습니다./SHA 변경: 이제 hash할 때 무작위 salt값과 같이 hash합니다 (2021-09-29)

--- src/front/src/components/MainComponent.vue
+++ src/front/src/components/MainComponent.vue
@@ -1,22 +1,25 @@
 <template>
   <div id="container">
     <div class="input-field">
-      <textarea v-model="encrypt.value" />
-      <div class="type-selection">
-        <select v-model="encrypt.type">
-          <option disabled value="">select encrypt option</option>
-          <option>SHA-224</option>
-          <option>SHA-256</option>
-          <option>SHA-384</option>
-          <option>SHA-512</option>
-          <option>AES-CBC</option>
-          <option>AES-GCM</option>
-        </select>
-        <button v-if="encrypt.type !== ''" @click="encryptValue()">encrypt</button>
-        <button v-if="encrypt.type.includes('AES')" @click="decryptValue()">decrypt</button>
-      </div>
-      <textarea readonly :value="encrypt.result" />
+      <textarea v-model="encrypt.value" placeholder="값 입력" required />
+      <textarea v-if="encrypt.type.includes('AES')" v-model="encrypt.iv" placeholder="initial vector 입력" required />
+      <textarea v-if="encrypt.type.includes('AES')" v-model="encrypt.key" placeholder="secretkey 입력" required />
     </div>
+    <div class="type-selection">
+      <select v-model="encrypt.type">
+        <option disabled value="">select encrypt option</option>
+        <option>SHA-224</option>
+        <option>SHA-256</option>
+        <option>SHA-384</option>
+        <option>SHA-512</option>
+        <option>AES-CBC</option>
+        <option>AES-GCM</option>
+      </select>
+      <button v-if="encrypt.type !== '' && !encrypt.type.includes('AES')" @click="encryptValue()" :disabled="encrypt.value === ''">encrypt</button>
+      <button v-if="encrypt.type !== '' && encrypt.type.includes('AES')" @click="encryptValue()" :disabled="encrypt.value === '' || encrypt.iv === '' || encrypt.key === ''">encrypt</button>
+      <button v-if="encrypt.type.includes('AES')" @click="decryptValue()" :disabled="encrypt.value === '' || encrypt.iv === '' || encrypt.key === ''">decrypt</button>
+    </div>
+    <textarea readonly :value="encrypt.result" />
   </div>
 </template>
 
@@ -30,17 +33,50 @@ export default {
     const encrypt = ref({
       type: '',
       value: '',
+      iv: '',
+      key: '',
       result: '',
     });
 
     const encryptValue = async () => {
-      let { data } = await axios.get(`/encrypt/${encrypt.value.type}?param=${encodeURIComponent(encrypt.value.value)}`);
-      encrypt.value.result = data;
+      if(encrypt.value.iv !== '' && checkByteLength(encrypt.value.iv) !== 16) {
+        alert(`해당 값은 ${16}byte 값을 가져야 합니다! (현재 ${checkByteLength(encrypt.value.iv)}byte)`);
+        return;
+      }
+
+      if(encrypt.value.key !== '' && checkByteLength(encrypt.value.key) !== 32) {
+        alert(`해당 값은 ${32}byte 값을 가져야 합니다! (현재 ${checkByteLength(encrypt.value.key)}byte)`);
+        return;
+      }
+
+      if(encrypt.value.iv === '' && encrypt.value.key === '') {
+        let { data } = await axios.get(`/encrypt/${encrypt.value.type}?param=${encodeURIComponent(encrypt.value.value)}`);
+        encrypt.value.result = data;
+      } else if(encrypt.value.iv !== '' && encrypt.value.key !== '') {
+        let { data } = await axios.get(`/encrypt/${encrypt.value.type}?param=${encodeURIComponent(encrypt.value.value)}&iv=${encodeURIComponent(encrypt.value.iv)}&key=${encodeURIComponent(encrypt.value.key)}`);
+        encrypt.value.result = data;
+      }
     };
     const decryptValue = async () => {
-      let { data } = await axios.get(`/decrypt/${encrypt.value.type}?param=${encodeURIComponent(encrypt.value.value)}`);
+      if(encrypt.value.iv !== '' && checkByteLength(encrypt.value.iv, 16) !== 16) {
+        alert(`해당 값은 ${16}byte 값을 가져야 합니다! (현재 ${checkByteLength(encrypt.value.iv, 16)}byte)`);
+        return;
+      }
+
+      if(encrypt.value.key !== '' && checkByteLength(encrypt.value.key, 32) !== 32) {
+        alert(`해당 값은 ${32}byte 값을 가져야 합니다! (현재 ${checkByteLength(encrypt.value.key, 32)}byte)`);
+      }
+
+      let { data } = await axios.get(`/decrypt/${encrypt.value.type}?param=${encodeURIComponent(encrypt.value.value)}&iv=${encodeURIComponent(encrypt.value.iv)}&key=${encodeURIComponent(encrypt.value.key)}`);
       encrypt.value.result = data;
     };
+    const checkByteLength = (value) => {
+      let result = 0;
+
+      for(let i = 0; i < value.length; i++) value.charCodeAt(i) > 127 ? result += 2 : result ++;
+
+      return result;
+    }
 
     return { encrypt, encryptValue, decryptValue };
   }
@@ -52,22 +88,18 @@ export default {
   width: 80%;
   margin: 0 auto;
   height: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
 }
 
 .input-field {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  height: 100%;
+  display: grid;
+  grid-template-rows: repeat(3, 1fr);
 }
 
 .type-selection {
   display: flex;
-  height: 10%;
-}
-
-textarea {
-  width: 30%;
-  height: 20%;
+  justify-content: space-around;
+  align-items: center;
 }
 </style>
--- src/main/java/com/gujerbit/encrypt/controller/EncryptController.java
+++ src/main/java/com/gujerbit/encrypt/controller/EncryptController.java
@@ -3,6 +3,7 @@ package com.gujerbit.encrypt.controller;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.util.Base64;
+import java.util.Random;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.GCMParameterSpec;
@@ -19,16 +20,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class EncryptController {
 	
-	private final String IV = "iloveprogramming";
-
 	@GetMapping("/encrypt/SHA-224")
 	public @ResponseBody String encryptSHA224(HttpServletRequest req) {
 		try {
 			String value = req.getParameter("param");
 			MessageDigest md = MessageDigest.getInstance("SHA-224");
-			byte[] messageDigest = md.digest(value.getBytes());
+			byte[] messageDigest = md.digest((value + createSalt()).getBytes()); //salt랑 같이 digest
+			//new BigInteger 앞에 숫자 -1,0,1만 됨
+			//-1 = 음수, 1 = 양수
+			//1을 해주는 이유는 byte를 unsigned 해주기 위해
+			//unsigned = 오직 양수만 허용됨
+			//암호화할 때는 무조건 unsigned
+			//signed는 -128~127 범위
+			//unsigned는 0~255 범위
+			//헥사코드로 표현해야하기 때문에 16진수인 unsigned
 			BigInteger bi = new BigInteger(1, messageDigest);
-			String hashText = bi.toString(16);
+			String hashText = bi.toString(16); //16진수로 변환하여 헥사코드로 표현
 			
 			while(hashText.length() < 32) hashText = "0" + hashText;
 			
@@ -44,9 +51,8 @@ public class EncryptController {
 		try {
 			String value = req.getParameter("param");
 			MessageDigest md = MessageDigest.getInstance("SHA-256");
-			md.update(value.getBytes());
 			
-			return String.format("%064x", new BigInteger(1, md.digest()));
+			return String.format("%064x", new BigInteger(1, md.digest((value + createSalt()).getBytes()))); //총 64자리의 16진수에서 값이 들어가지 못한 자리는 0으로 채움
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
@@ -58,8 +64,7 @@ public class EncryptController {
 		try {
 			String value = req.getParameter("param");
 			MessageDigest md = MessageDigest.getInstance("SHA-384");
-			md.update(value.getBytes());
-			byte[] messageDigest = md.digest();
+			byte[] messageDigest = md.digest((value + createSalt()).getBytes());
 			
 			return new BigInteger(1, messageDigest).toString(16);
 		} catch (Exception e) {
@@ -73,26 +78,27 @@ public class EncryptController {
 		try {
 			String value = req.getParameter("param");
 			MessageDigest md = MessageDigest.getInstance("SHA-512");
-			md.update(value.getBytes());
 			
-			return String.format("%0128x", new BigInteger(1, md.digest()));
+			return String.format("%0128x", new BigInteger(1, md.digest((value + createSalt()).getBytes()))); //128자리의 16진수에서 값이 들어가지 못한 자리는 0으로 채움
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
 	
-	@GetMapping("/encrypt/AES-CBC")
+	@GetMapping("/encrypt/AES-CBC") //256 = key length -> 32byte, iv는 키 값과 무관하게 16byte
 	public @ResponseBody String encryptAESCBC(HttpServletRequest req) {
 		try {
 			String value = req.getParameter("param");
+			String iv = req.getParameter("iv");
+			String key = req.getParameter("key");
 			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-			SecretKeySpec ss = new SecretKeySpec(IV.getBytes(), "AES");
-			IvParameterSpec ips = new IvParameterSpec(IV.getBytes());
-			cipher.init(Cipher.ENCRYPT_MODE, ss, ips);
-			byte[] encrypt = cipher.doFinal(value.getBytes());
+			SecretKeySpec ss = new SecretKeySpec(key.getBytes(), "AES"); //지정된 알고리즘으로부터 바이트 배열을 넘겨받아 키 구축
+			IvParameterSpec ips = new IvParameterSpec(iv.getBytes()); //iv값 생성
+			cipher.init(Cipher.ENCRYPT_MODE, ss, ips); //암호화 모드로 초기화
+			byte[] encrypt = cipher.doFinal(value.getBytes()); //초기화 모드에 따라서 작업
 			
-			return new String(Base64.getEncoder().encode(encrypt));
+			return new BigInteger(1, encrypt).toString(16); //16진수로 변환
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
@@ -103,12 +109,14 @@ public class EncryptController {
 	public @ResponseBody String decryptAESCBC(HttpServletRequest req) {
 		try {
 			String value = req.getParameter("param");
+			String iv = req.getParameter("iv");
+			String key = req.getParameter("key");
 			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-			SecretKeySpec ss = new SecretKeySpec(IV.getBytes(), "AES");
-			IvParameterSpec ips = new IvParameterSpec(IV.getBytes());
-			cipher.init(Cipher.DECRYPT_MODE, ss, ips);
-			byte[] decryptByte = Base64.getDecoder().decode(value.getBytes());
-			byte[] decrypt = cipher.doFinal(decryptByte);
+			SecretKeySpec ss = new SecretKeySpec(key.getBytes(), "AES");
+			IvParameterSpec ips = new IvParameterSpec(iv.getBytes());
+			cipher.init(Cipher.DECRYPT_MODE, ss, ips); //복호화 모드로 초기화
+			byte[] hexResult = new BigInteger(value, 16).toByteArray(); //16진수를 바이트로 변환
+			byte[] decrypt = cipher.doFinal(hexResult);
 			
 			return new String(decrypt);
 		} catch (Exception e) {
@@ -117,17 +125,19 @@ public class EncryptController {
 		}
 	}
 	
-	@GetMapping("/encrypt/AES-GCM")
+	@GetMapping("/encrypt/AES-GCM") //256 = key length -> 32byte, iv는 키 값과 무관하게 16byte
 	public @ResponseBody String encryptAESGCM(HttpServletRequest req) {
 		try {
 			String value = req.getParameter("param");
+			String iv = req.getParameter("iv");
+			String key = req.getParameter("key");
 			Cipher cipher = Cipher.getInstance("AES/GCM/PKCS5Padding");
-			SecretKeySpec ss = new SecretKeySpec(IV.getBytes(), "AES");
-			GCMParameterSpec gps = new GCMParameterSpec(128, IV.getBytes());
+			SecretKeySpec ss = new SecretKeySpec(key.getBytes(), "AES");
+			GCMParameterSpec gps = new GCMParameterSpec(128, iv.getBytes()); //지정된 태그의 길이 및 iv값을 사용
 			cipher.init(Cipher.ENCRYPT_MODE, ss, gps);
 			byte[] encrypt = cipher.doFinal(value.getBytes());
 			
-			return new String(Base64.getEncoder().encode(encrypt));
+			return new BigInteger(1, encrypt).toString(16);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
@@ -138,18 +148,34 @@ public class EncryptController {
 	public @ResponseBody String decryptAESGCM(HttpServletRequest req) {
 		try {
 			String value = req.getParameter("param");
+			String iv = req.getParameter("iv");
+			String key = req.getParameter("key");
 			Cipher cipher = Cipher.getInstance("AES/GCM/PKCS5Padding");
-			SecretKeySpec ss = new SecretKeySpec(IV.getBytes(), "AES");
-			GCMParameterSpec gps = new GCMParameterSpec(128, IV.getBytes());
+			SecretKeySpec ss = new SecretKeySpec(key.getBytes(), "AES");
+			GCMParameterSpec gps = new GCMParameterSpec(128, iv.getBytes());
 			cipher.init(Cipher.DECRYPT_MODE, ss, gps);
-			byte[] decryptByte = Base64.getDecoder().decode(value.getBytes());
-			byte[] decrypt = cipher.doFinal(decryptByte);
+			byte[] hexResult = new BigInteger(value, 16).toByteArray();
+			byte[] decrypt = cipher.doFinal(hexResult);
 			
 			return new String(decrypt);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
+	}
+	
+	private String createSalt() {
+		Random rnd = new Random();
+		String result = "";
+		
+		for(int i = 0; i < 32; i++) {
+			int select = rnd.nextInt(3); //0~2
+			if(select == 0) result += String.valueOf((char) (rnd.nextInt(26) + 65)); //ASCII 변환 대문자 65~90까지
+			else if(select == 1) result += String.valueOf((char) (rnd.nextInt(26) + 97)); //소문자 97부터 122까지
+			else result += String.valueOf(rnd.nextInt(10)); //0~9
+		}
+		
+		return result;
 	}
 	
 }
